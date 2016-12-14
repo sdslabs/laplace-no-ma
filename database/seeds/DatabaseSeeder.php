d3024bbde33787d7619ec5fa7f0c5f6a762f7d88
Asutosh Palai <asupalai@gmail.com>
2016-12-14 13:47:32 +0530

    Added Falcon and DB seeder from quark

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use SDSLabs\Quark\Database\Seeds\DatabaseSeeder as QuarkSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(QuarkSeeder::class);
     }
 }
